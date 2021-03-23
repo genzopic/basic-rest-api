7813fb161001372a1aaf267e39ea617427ce18de
genzo <genzo.pic@gmail.com>
2021-03-23 21:50:34 +0900

    feture update,delete user

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,6 +1,6 @@
 const indexModule = (() => {
   const path = window.location.pathname
-  console.log("path: " + path)
+  //console.log("path: " + path)
 
   switch (path) {
     case '/':
@@ -11,7 +11,6 @@ const indexModule = (() => {
 
         // UsersモジュールのfetchAllUsersメソッドを呼び出す
         return usersModule.fetchAllUsers()
-
     case '/create.html':
       document.getElementById('save-btn').addEventListener('click',() => {
         return usersModule.createUser()
@@ -19,7 +18,20 @@ const indexModule = (() => {
       document.getElementById('cancel-btn').addEventListener('click',() => {
         return window.location.href = '/'
       })
+      break
+    case '/edit.html':
+      const uid = window.location.search.split('?uid=')[1]
 
+      document.getElementById('save-btn').addEventListener('click',() => {
+        return usersModule.saveUser(uid)
+      })
+      document.getElementById('cancel-btn').addEventListener('click',() => {
+        return window.location.href = '/'
+      })
+      document.getElementById('delete-btn').addEventListener('click',() => {
+        return usersModule.deleteUser(uid)
+      })
+      return usersModule.setExistingValue(uid)
     default:
       break
   }
--- app/public/js/users.js
+++ app/public/js/users.js
@@ -21,6 +21,7 @@ const usersModule = (() => {
                         <td>${user.date_of_birth}</td>
                         <td>${user.created_at}</td>
                         <td>${user.updated_at}</td>
+                        <td><a href="edit.html?uid=${user.id}">編集</a></td>
                       </tr>`
         // beforeend：末尾
         document.getElementById('users-list').insertAdjacentHTML('beforeend',body);
@@ -47,12 +48,71 @@ const usersModule = (() => {
         body: JSON.stringify(body)
       })
 
-      // 結果を受け取る
+      // 結果をJSONで受け取りメッセージを表示
       const resJSON = await res.json()
       alert(resJSON.message)
       
       // 一覧ページへ遷移する
       window.location.href = "/"
+    },
+    // ユーザ情報表示
+    setExistingValue: async (uid) => {
+      const res = await fetch(BASE_URL + "/" + uid)
+      const resJson = await res.json()
+
+      document.getElementById('name').value = resJson.name
+      document.getElementById('profile').value = resJson.profile
+      document.getElementById('date-of-birth').value = resJson.date_of_birth
+    },
+    // ユーザ追加
+    saveUser: async (uid) => {
+      const name = document.getElementById("name").value
+      const profile = document.getElementById("profile").value
+      const dateofbirth = document.getElementById("date_of_birth").value
+      console.log("createUser start")
+
+      // リクエストのbody
+      const body = {
+        name: name,
+        profile: profile,
+        date_of_birth: dateofbirth
+      }
+
+      // WebAPIを実行してユーザを追加する（PUT /api/v1/users/:uid）
+      const res = await fetch(BASE_URL + "/" + uid, {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify(body)
+      })
+
+      // 結果をJSONで受け取りメッセージを表示
+      const resJSON = await res.json()
+      alert(resJSON.message)
+      
+      // 一覧ページへ遷移する
+      window.location.href = "/"
+    },
+    // ユーザ削除
+    deleteUser: async(uid) => {
+      const ret = window.confirm('このユーザを削除しますか？')
+      if (!ret) {
+        return false
+      } else {
+        // WebAPIを実行してユーザを追加する（PUT /api/v1/users/:uid）
+        const res = await fetch(BASE_URL + "/" + uid, {
+          method: "PUT",
+          headers: headers
+        })
+
+        // 結果をJSONで受け取りメッセージを表示
+        const resJSON = await res.json()
+        alert(resJSON.message)
+        
+        // 一覧ページへ遷移する
+        window.location.href = "/"
+
+      }
+
     }
   }
 
